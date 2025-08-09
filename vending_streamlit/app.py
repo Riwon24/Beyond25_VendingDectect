afcd43c8bf48a1e2d19d99565f7d74681521f18f
Riwon24 <oatmeal24@sookmyung.ac.kr>
2025-08-09 23:07:55 +0900

    Update app.py

--- vending_streamlit/app.py
+++ vending_streamlit/app.py
@@ -1,15 +1,14 @@
 # A,B단계에서 가져올 부분
 from utils import preprocess_image, run_inference
-from visualize import yolov8_postprocess, extract_detected_items, draw_boxes
+from visualize import yolov8_postprocess, extract_detected_items, scale_boxes, draw_boxes
 
 import streamlit as st
-from PIL import Image
 import numpy as np
 import cv2
 
 # 모델 관련 불러오기
-import onnxruntime as ort
-session = ort.InferenceSession("best.onnx")
+import onnxruntime
+session = onnxruntime.InferenceSession("best.onnx")
 
 def uploaded(uploaded_file, session):
     # 바이트로 읽은 파일을 numpy 배열로 디코딩(cv2가 읽을 수 있게)
@@ -20,14 +19,16 @@ def uploaded(uploaded_file, session):
     input_tensor, original_img = preprocess_image(img)
     output = run_inference(session, input_tensor)  # A; 모델 추론 실행
 
-    # 후처리
-    boxes = yolov8_postprocess(output)
-
     with open("classes.txt", "r") as f:
         class_names = [line.strip() for line in f.readlines()]  # classes.txt에서 클래스 읽어옴
 
-    detected_items = extract_detected_items(boxes, class_names)  # B; 결과에서 품목 추출
-    result_img = draw_boxes(img, boxes, classes_names)  # B; 이미지 위에 박스그릠
+    # 후처리
+    boxes = yolov8_postprocess(output, conf_threshold=0.3 ,debug=True)
+
+    detected_items = extract_detected_items(boxes, class_names, conf_threshold=0.3)  # B; 결과에서 품목 추출
+
+    boxes_scaled = scale_boxes(boxes, input_shape=(640, 640), orig_shape=img.shape[:2])
+    result_img = draw_boxes(img, boxes_scaled, class_names, conf_threshold=0.3) # B; 이미지 위에 박스그릠
 
     return detected_items, result_img, boxes
 
@@ -42,7 +43,3 @@ if uploaded_file:
     detected_items, result_img, boxes = uploaded(uploaded_file, session)
     st.image(result_img, caption="탐지 결과")
     st.write("탐지된 품목:", detected_items)
-
-    # 디버그
-    st.write('모델 입력 텐서 이름: ' ,session.get_inputs()[0].name)
-    st.write("후처리 결과 박스 수:", len(boxes))
