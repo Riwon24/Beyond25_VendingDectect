74fef81d4579ebfc213d1add80c3418a46fdd278
Riwon24 <oatmeal24@sookmyung.ac.kr>
2025-08-09 23:49:43 +0900

    Update app.py

--- vending_streamlit/app.py
+++ vending_streamlit/app.py
@@ -22,13 +22,13 @@ def uploaded(uploaded_file, session):
     with open("classes.txt", "r") as f:
         class_names = [line.strip() for line in f.readlines()]  # classes.txt에서 클래스 읽어옴
 
-    # 후처리
-    boxes = yolov8_postprocess(output, conf_threshold=0.3 ,debug=True)
+    # 후처리(conf_threshold=0.35로 설정)
+    boxes = yolov8_postprocess(output, conf_threshold=0.35 ,debug=False)
 
-    detected_items = extract_detected_items(boxes, class_names, conf_threshold=0.3)  # B; 결과에서 품목 추출
+    detected_items = extract_detected_items(boxes, class_names, conf_threshold=0.35)  # B; 결과에서 품목 추출
 
     boxes_scaled = scale_boxes(boxes, input_shape=(640, 640), orig_shape=img.shape[:2])
-    result_img = draw_boxes(img, boxes_scaled, class_names, conf_threshold=0.3) # B; 이미지 위에 박스그릠
+    result_img = draw_boxes(img, boxes_scaled, class_names, conf_threshold=0.35) # B; 이미지 위에 박스그릠
 
     return detected_items, result_img, boxes
 
@@ -42,4 +42,6 @@ uploaded_file = st.file_uploader("Upload an image for item detection using an ON
 if uploaded_file:
     detected_items, result_img, boxes = uploaded(uploaded_file, session)
     st.image(result_img, caption="탐지 결과")
-    st.write("탐지된 품목:", detected_items)
+    st.subheader("Detected item:")
+
+    st.markdown(f"**{", ".join(detected_items)}**")
